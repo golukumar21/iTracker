808d1888c221b85b93fe0700dbfa8d07f1774942
Kumar Abhishek <golukumar21@gmail.com>
2019-08-27 17:21:02 +0530

    project details creation page

--- src/createProjectDetails/createProjectDet.js
+++ src/createProjectDetails/createProjectDet.js
@@ -2,10 +2,53 @@ import React from 'react';
 import './createProjectDet.css';
 
 class createProjectDet extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        }
+    }
     render() {
-        return(
+        return (
             <div className="create_prjDet">
-                CREATE PROJECT
+                <h1>Create New Project</h1>
+                <div className="prj_det">
+                    <div className="prj_name"> 
+                        <label>Project Name:</label>
+                        <input type="text" placeholder="Enter project name" />
+                    </div>
+                    <div className="prj_mgr">
+                        <label>Reporting Manager:</label>
+                        <input type="text" placeholder="Enter manager name" />
+                    </div>
+                    <div className="prj_status">
+                        <label>Project Status:</label>
+                        <select>
+                            <option>Yet to Start</option>
+                            <option>Started</option>
+                            <option>In Progress</option>
+                            <option>On Hold</option>
+                            <option>Completed</option>
+                        </select>
+                    </div>
+                    <div className="prj_create_date">
+                        <label>Date Created:</label>
+                        <input type="text" readOnly="readOnly" placeholder="Enter manager name" value={this.state.date}/>
+                    </div>
+                    <div className="prj_eta">
+                        <label>E T A:</label>
+                        <input type="date" />
+                    </div>
+                    <div className="prj_resource">
+                        <label>No. of Resources:</label>
+                        <input type="number" placeholder="Enter number of resources engaged" />
+                    </div>
+                    <div className="prj_desc">
+                        <label>Project Description:</label>
+                        <textarea placeholder="Enter project description"></textarea>
+                    </div>
+                </div>
+                <button>Submit</button>
             </div>
         )
     }
